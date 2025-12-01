a854bf19cdae4a7ef40657616648b6f9fbd830fb
ktrikha2 <keshav.trikha@gmail.com>
2025-12-01 17:05:36 -0600

    fixing transforms

--- src/Dataset/Transform.py
+++ src/Dataset/Transform.py
@@ -11,13 +11,26 @@ def random_horizontal_flip(image, target, p=0.5):
         if 'boxes' in target and target['boxes'].numel() > 0:
             w = image.shape[2]
             boxes = target['boxes'].clone()
-            boxes[:, [0, 2]] = w - boxes[:, [2, 0]]
+            boxes[:, 0] = w - (boxes[:, 0] + boxes[:, 2])
             target['boxes'] = boxes
     return image, target
+
+
+def filter_invalid_boxes(target):
+    boxes = target["boxes"]
+
+    # Keep only boxes with positive width & height
+    keep = (boxes[:, 2] > 1) & (boxes[:, 3] > 1)
+
+    target["boxes"]  = boxes[keep]
+    target["labels"] = target["labels"][keep]
+
+    return target
 
 
 def compose_transforms():
     def transform(image, target):
         image, target = random_horizontal_flip(image, target, p=0.5)
+        target = filter_invalid_boxes(target)
         return image, target
     return transform
