983e63f65172448b36df5ed3df96a09d64316075
ktrikha2 <keshav.trikha@gmail.com>
2025-12-05 11:58:50 -0600

    testing with new debugging MAP2

--- evaluation.py
+++ evaluation.py
@@ -334,7 +334,7 @@ def evaluate_model(pred_file, gt_file, img_dir, output_dir="evaluation_results",
 
 if __name__ == "__main__":
     # Configure paths
-    PRED_FILE = " val_predictions_swin_detr_epochdebug.json"  # Change to your prediction file
+    PRED_FILE = "val_predictions_swin_detr_epochdebug.json"  # Change to your prediction file
     GT_FILE = "/work/nvme/bfdu/ktrikha/code/cs444-final/Data/det_val_10k.json"
     IMG_DIR = "/work/nvme/bfdu/ktrikha/data/Dataset/val"
     OUTPUT_DIR = "evaluation_results_debug4"
