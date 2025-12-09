ab785b727e2c7fe72d669f1d3a45285de72a7458
ktrikha2 <keshav.trikha@gmail.com>
2025-12-08 20:58:31 -0600

    eval2

--- evaluation.py
+++ evaluation.py
@@ -334,10 +334,10 @@ def evaluate_model(pred_file, gt_file, img_dir, output_dir="evaluation_results",
 
 if __name__ == "__main__":
     # Configure paths
-    PRED_FILE = "resultsdebug/swin_detr_5.json"  # Change to your prediction file
+    PRED_FILE = "resultsdbug2/swin_detr_5.json"  # Change to your prediction file
     GT_FILE = "/work/nvme/bfdu/ktrikha/code/cs444-final/Data/det_val_10k.json"
     IMG_DIR = "/work/nvme/bfdu/ktrikha/data/Dataset/val"
-    OUTPUT_DIR = "evaluation_results_debug_full"
+    OUTPUT_DIR = "evaluation_results_debug_test"
     
     # IMPORTANT: Limit to just 200 images (your validation subset)
     MAX_IMAGES = None
